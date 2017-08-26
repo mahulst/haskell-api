72dd2884c0c75c4253ac3b3341cba375417fb393
Michel van der Hulst <michel@voorkanter.com>
2017-08-26 13:27:17 +0200

    Working API with QueryParams

---
+++ src/Lib.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+module Lib
+    ( startApp
+    , app
+    ) where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.API
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Control.Monad.Trans.Either
+import Data.Text
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+data SortBy = Age | Name
+
+instance FromHttpApiData SortBy where
+  parseQueryParam text =
+    case unpack(text)  of
+      "age" -> Right Age
+      "name" -> Right Name
+      _ -> Left (pack "None")
+
+type API = "users" :> QueryParam "sorty" SortBy :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = users
+  where users :: Maybe SortBy -> Handler [User]
+        users sort =
+          case sort of
+            Just Age -> return userList
+            Just Name -> return (Prelude.reverse userList)
+            Nothing -> return []
+userList :: [User]
+userList = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
